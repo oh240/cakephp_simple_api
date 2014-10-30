b80c5a7cbe43eec332210c59b6a655c3b3c35352
Nishio Takuya <littlecub240@gmail.com>
2014-10-30 16:32:27 +0900

    initial commit

---
+++ Controller/PostsController.php
@@ -0,0 +1,61 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Posts Controller
+ *
+ */
+class PostsController extends AppController {
+
+	public function index() {
+		$posts = $this->Post->find('all');
+		$this->set(array(
+			'posts' => $posts,
+			'_serialize' => array('posts')
+		));
+	}
+
+	public function view($id) {
+		$post = $this->Post->findById($id);
+		$this->set(array(
+			'post' => $post,
+			'_serialize' => array('post')
+		));
+	}
+
+	public function add(){
+		if ($this->Post->save($this->request->data)) {
+			$message = 'Saved';
+		} else {
+			$message = 'Error';
+		}
+		$this->set(array(
+			'message' => $message,
+			'_serialize' => array('message')
+		));
+	}
+
+	public function edit($id) {
+		$this->Post->id = $id;
+		if ($this->Post->save($this->request->data)) {
+			$message = 'Saved';
+		} else {
+			$message = 'Error';
+		}
+		$this->set(array(
+			'message' => $message,
+			'_serialize' => array('message')
+		));
+	}
+
+	public function delete($id) {
+		if ($this->Post->delete($id)) {
+			$message = 'Deleted';
+		} else {
+			$message = 'Error';
+		}
+		$this->set(array(
+			'message' => $message,
+			'_serialize' => array('message')
+		));
+	}
+}
